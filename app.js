210537a261f96f650ca5b7ed77da12407ad257f9
Maikel Esquivel <marcostorres04031997@gmail.com>
2021-08-06 13:24:46 -0300

    footer

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.use(express.static('public'));
 
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
-app.get('/Ofertas', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/ofertas', (req,res) => res.sendFile(path.join(__dirname, 'views', 'ofertas.html')));
 
 app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port));
