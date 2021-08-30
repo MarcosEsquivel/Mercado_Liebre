4cb786480fd13573d8eec481229ad1eef11819ad
Maikel Esquivel <marcostorres04031997@gmail.com>
2021-08-30 01:30:57 -0300

    acomodando nav y logo

--- app.js
+++ app.js
@@ -9,4 +9,4 @@ app.use(express.static('public'));
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
 
 
-app.listen(port, () => console.log('Servidor corriendo en http://localhost: ' + port));
+app.listen(port, () => console.log('Servidor corriendo en http://localhost:' + port));
